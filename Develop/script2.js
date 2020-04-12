6355e529baea786934bee31a8e7114c6342f9082
Dan Weikart <danmweikart@gmail.com>
2020-04-12 15:52:18 -0700

    Added functionality to checkboxes. Add item to fears conquered list. Still need to remove from current list.

--- Develop/script2.js
+++ Develop/script2.js
@@ -1,4 +1,4 @@
-var fearsOfMonthList = JSON.parse(localStorage.getItem("fearsOfMonthLocal")) || [];
+var fearsOfMonthList = JSON.parse(localStorage.getItem("fearsOfMonthLocal")) || []; 
 var fearsOfMonthListEl = $("#monthList");
 var fearsOfMonthNew = ""
 
@@ -6,6 +6,9 @@ var fearOfDayList = JSON.parse(localStorage.getItem("fearOfDayLocal")) || [];
 var fearOfDayListEl = $("#dayList");
 var fearOfDayNew = ""
 
+var fearsConqueredList = JSON.parse(localStorage.getItem("fearsConqueredLocal")) || [];
+var fearsConqueredListEl = $("#conqueredList");
+var fearsConqueredNew = ""
 
 renderFearsOfMonth();
 renderFearOfDay();
@@ -25,12 +28,11 @@ function renderFearsOfMonth () {
 $("#fearsOfMonthBtn").on("click", function (event){
     event.preventDefault();
     fearsOfMonthNew = $("#fearsOfMonthText").val();
-    if (fearsOfMonthList.indexOf(fearsOfMonthNew) === -1) {
-        fearsOfMonthList.push(fearsOfMonthNew);
-        localStorage.setItem("fearsOfMonthLocal", JSON.stringify(fearsOfMonthList));
+    if (fearsOfMonthList.indexOf(fearsOfMonthNew) === -1) {                          //if fearsOfMonthList is empty
+        fearsOfMonthList.push(fearsOfMonthNew);                                      //add to list
+        localStorage.setItem("fearsOfMonthLocal", JSON.stringify(fearsOfMonthList)); //set localStorage
     }
     renderFearsOfMonth();
-
 })
 
 function renderFearOfDay () {
@@ -53,5 +55,47 @@ $("#fearOfDayBtn").on("click", function (event){
         localStorage.setItem("fearOfDayLocal", JSON.stringify(fearOfDayList));
     }
     renderFearOfDay();
-
 })
+
+function renderFearsConquered () {
+    $("#conqueredList").empty();
+    for (var i = 0; i < fearsConqueredList.length; i++) {
+        fearOfDayListEl = fearOfDayList.join();
+        var fearsConqueredListItem = $("<li>");
+        fearsConqueredListItem.text(fearsConqueredList[i]);
+        $("#conqueredList").append(fearsConqueredListItem);
+    }
+}
+
+$(document).ready(function(){
+    $('input[type="checkbox"]').click(function(){
+        if($(this).prop("checked") == true){
+            fearsConqueredNew = $(this).parent().text();
+            fearsConqueredList.push(fearsConqueredNew);
+            console.log(fearsConqueredNew);
+   
+    if (fearsConqueredList.indexOf(fearsConqueredNew) === -1) {
+        fearsConqueredList.push(fearsConqueredNew);
+         localStorage.setItem("fearsConqueredLocal", JSON.stringify(fearsConqueredList));
+    }
+}
+    renderFearsConquered();
+    }
+    )
+});
+
+// Possible drag and drop feature code:
+
+// function allowDrop(ev) {
+//     ev.preventDefault();
+//   }
+  
+//   function drag(ev) {
+//     ev.dataTransfer.setData("text", ev.target.id);
+//   }
+  
+//   function drop(ev) {
+//     ev.preventDefault();
+//     var data = ev.dataTransfer.getData("text");
+//     ev.target.append($("#data"));
+//   }
