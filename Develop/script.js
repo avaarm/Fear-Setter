576b6d4d7794d5ae1f995fa04e9db5df633b0a84
Seth Purr <eric.purrington@gmail.com>
2020-04-16 19:13:01 -0700

    added apis to index and changed index navbars

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,23 @@
-// Inspirational Quote API
-$.ajax({
+$(".uk-button-small").on("click", function () {
+
+  // Inspirational Quote API
+  $.ajax({
     url: "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand",
     method: "GET"
   }).then( function(response) {
     var quote = $(response[0].content.rendered);
-    // $(".dummydiv").append(quote);
+    $(".quote").append(quote);
   });
 
-// Giphy API
-$.ajax({
+  // Giphy API
+  $.ajax({
     url: "https://api.giphy.com/v1/gifs/random?api_key=" + config.giphyAPI + "&tag=cute",
     method: "GET"
   }).then(function (response) {
     var imageUrl = response.data.image_original_url;
     var randGIF = $("<img>");
     randGIF.attr("src", imageUrl).attr("alt", "random image");
-    // $(".dummydiv").append(randGIF);
+    $(".gif").append(randGIF);
   });
+
+});
